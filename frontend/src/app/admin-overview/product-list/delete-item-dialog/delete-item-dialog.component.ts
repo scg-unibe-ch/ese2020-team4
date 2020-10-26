b3d4c116ad3fdbfedd7a733f7acc1d94fb9746e0
Julien Brunner <julien.brunner@students.unibe.ch>
2020-10-26 11:57:21 +0100

    delete products by admin frontend/backend implemented

---
+++ frontend/src/app/admin-overview/product-list/delete-item-dialog/delete-item-dialog.component.ts
@@ -0,0 +1,33 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+
+@Component({
+  selector: 'app-delete-item-dialog',
+  templateUrl: './delete-item-dialog.component.html',
+  styleUrls: ['./delete-item-dialog.component.css']
+})
+export class DeleteItemDialogComponent implements OnInit {
+
+  public constructor(public dialogRef: MatDialogRef<DeleteItemDialogComponent>,
+                     @Inject(MAT_DIALOG_DATA) public data: number, private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  close() {
+    this.dialogRef.close()
+  }
+
+  save() {
+    console.log(this.data)
+    this.httpClient.delete(environment.endpointURL + 'item/delete/' + this.data ).subscribe((res: any) => {
+      console.log('frontend 1');
+    });
+    this.dialogRef.close();
+
+  }
+}
